bf922950a57e6dbf1875c5a35dc9b6490ac17c26
Terence Lee <hone02@gmail.com>
2010-12-29 03:07:55 -0600

    handle an empty inbox

--- lib/jobs/imap_job.rb
+++ lib/jobs/imap_job.rb
@@ -11,24 +11,27 @@ class ImapJob
     imap.select("INBOX")
 
     archive_messages = []
-    imap.fetch(1..-1, "(UID BODY.PEEK[HEADER.FIELDS (SUBJECT)] BODY.PEEK[HEADER.FIELDS (TO)])").each do |message|
-      mid     = message.seqno
-      uid     = message.attr["UID"]
-      subject = message.attr["BODY[HEADER.FIELDS (SUBJECT)]"].sub("Subject ", '').chomp.chomp
-      folder  = Mail::Address.new(message.attr["BODY[HEADER.FIELDS (TO)]"].sub("To: ", '').chomp.chomp).local.capitalize
+    messages = imap.fetch(1..-1, "(UID BODY.PEEK[HEADER.FIELDS (SUBJECT)] BODY.PEEK[HEADER.FIELDS (TO)])")
+    if messages
+      messages.each do |message|
+        mid     = message.seqno
+        uid     = message.attr["UID"]
+        subject = message.attr["BODY[HEADER.FIELDS (SUBJECT)]"].sub("Subject ", '').chomp.chomp
+        folder  = Mail::Address.new(message.attr["BODY[HEADER.FIELDS (TO)]"].sub("To: ", '').chomp.chomp).local.capitalize
 
-      if not imap.list('OtherInbox/', folder)
-        puts "Creating folder OtherInbox/#{folder}"
-        imap.create("OtherInbox/#{folder}")
+        if not imap.list('OtherInbox/', folder)
+          puts "Creating folder OtherInbox/#{folder}"
+          imap.create("OtherInbox/#{folder}")
+        end
+
+        puts "Processing #{uid} : #{subject} to #{folder}"
+        imap.copy(mid, "OtherInbox/#{folder}")
+        archive_messages << mid
       end
-
-      puts "Processing #{uid} : #{subject} to #{folder}"
-      imap.copy(mid, "OtherInbox/#{folder}")
-      archive_messages << mid
     end
 
     # archive email
-    imap.store(archive_messages, "+FLAGS", [:Deleted])
+    imap.store(archive_messages, "+FLAGS", [:Deleted]) if archive_messages.any?
 
     imap.disconnect
   end
