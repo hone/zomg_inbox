ac9e09cada2ea4d682b388c54a539a1416e4fa54
Terence Lee <hone02@gmail.com>
2010-12-29 02:53:04 -0600

    heroku is overriding the ENV var PORT

--- clock.rb
+++ clock.rb
@@ -6,5 +6,5 @@ require File.join(File.dirname(__FILE__), 'lib/jobs')
 include Clockwork
 
 every(5.minutes, 'run ImapJob') do |job|
-  Resque.enqueue(ImapJob, ENV['HOST'], ENV['PORT'], ENV['SSL'] == 'true' ? true : false, ENV['USERNAME'], ENV['PASSWORD'])
+  Resque.enqueue(ImapJob, ENV['HOST'], ENV['IMAP_PORT'], ENV['SSL'] == 'true' ? true : false, ENV['USERNAME'], ENV['PASSWORD'])
 end
