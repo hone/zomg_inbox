d705f01aca7a8d8733e59c4319632dba7437fdab
Terence Lee <hone02@gmail.com>
2011-02-21 21:48:12 -0600

    use global CONFIG constant

--- config/redis_setup.rb
+++ config/redis_setup.rb
@@ -2,4 +2,7 @@ require 'resque'
 
 redis_url    = ENV["REDISTOGO_URL"] ||= "redis://localhost:6379/"
 uri          = URI.parse(redis_url)
-Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+
+CONFIG ||= {}
+CONFIG[:redis] = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+Resque.redis = CONFIG[:redis]
